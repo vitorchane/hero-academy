bc342746f0656a88b3a8d0aaedae37d029a4604f
Vitor Chane <vitor.chane@iesde.com.br>
2026-04-16 15:43:30 -0300

    feat: Adding dto validators

--- hero-academy-backend/src/heroes/dto/create-hero.dto.ts
+++ hero-academy-backend/src/heroes/dto/create-hero.dto.ts
@@ -1,25 +1,44 @@
 import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsBoolean,
+  IsDateString,
+  IsEnum,
+  IsNotEmpty,
+  IsString,
+} from 'class-validator';
 import { Power, Universe } from '../../generated/prisma/enums';
 
 export class CreateHeroDto {
   @ApiProperty({ example: 'Peter Parker' })
+  @IsString()
+  @IsNotEmpty()
   name: string;
 
   @ApiProperty({ example: 'Homem-Aranha' })
+  @IsString()
+  @IsNotEmpty()
   nickname: string;
 
   @ApiProperty({ example: '2001-08-10T00:00:00.000Z' })
+  @IsDateString()
+  @IsNotEmpty()
   date_of_birth: Date;
 
   @ApiProperty({ enum: Universe, example: Universe.MARVEL })
+  @IsEnum(Universe)
+  @IsNotEmpty()
   universe: Universe;
 
   @ApiProperty({ enum: Power, example: Power.LANCAMENTO_DE_TEIAS })
+  @IsEnum(Power)
+  @IsNotEmpty()
   main_power: Power;
 
   @ApiProperty({ example: 'https://example.com/spiderman.png' })
+  @IsNotEmpty()
   avatar_url: string;
 
   @ApiProperty({ example: true })
+  @IsBoolean()
   is_active: boolean;
 }
